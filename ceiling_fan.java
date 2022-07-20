0a47fb9bb2a61f82b8e58956bcbd3efd001df4a4
Jay-M <jean_marcc@hotmail.com>
2022-07-19 23:17:24 -0300

    user prompt instructions added

--- ceiling_fan.java
+++ ceiling_fan.java
@@ -3,6 +3,9 @@ import java.util.*;
 public class ceiling_fan{
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
+        System.out.println("If you want to change the ceiling settings please select one of the 2 cords. \nPress 1: to increase fan speed.\nPress 2: to reverse the direction of current speed setting (can only be undone by pressing 2 again).\nPress 3: to quit.");
         String selectCord = scan.nextLine();
+
+        
     }
 }
