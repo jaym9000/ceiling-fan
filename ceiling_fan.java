47baaf12ed7e7cf05987ea0ea55d2aef74c4f7bc
Jay-M <jean_marcc@hotmail.com>
2022-07-20 22:28:01 -0300

    Using boolean for reverse pull cord, still figuring out the 3 speed settings and off loop

--- ceiling_fan.java
+++ ceiling_fan.java
@@ -3,16 +3,35 @@ import java.util.*;
 public class ceiling_fan{
 
     static boolean quit = false;
+    static int fanSpeed = 0;
+    static boolean reversed = false;
 
     static void speedCord(){
         // Increase the speed each Time cord is pulled.
         // (There are 3 speed settings and an "off" [speed 0] setting). If the cord is pulled on speed 3, the fan returns to the "off" setting.
-        System.out.println("Pressed 1");
+
+        if(reverseCord() == false){
+            if(fanSpeed == 3){
+                fanSpeed = 0;
+            }else
+            fanSpeed++;
+        }else{
+            if(fanSpeed == 0){
+                fanSpeed = 3;
+            }
+            fanSpeed--;
+        }
+        if(fanSpeed != 0){
+            System.out.println("Updated fan speed is now:" + fanSpeed);
+        }else
+            System.out.println("Fan is now off");
     }
 
-    static void reverseCord(){
+    static boolean reverseCord(){
         // Reverses the direction of the fan at the current speed setting. Once the direction has been reversed, it remains reversed as we cycle through the speed settings, until reversed again.
-        System.out.println("Pressed 2");
+
+        // if false, reverse NOT activated. if true, reverse IS activated.
+        return reversed = !reversed; 
     }
 
     public static void main(String[] args) {
